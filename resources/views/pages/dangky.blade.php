01f2336ce5678d8f5856d53e27a52f570d9f7718
Hao <kehao12@gmail.com>
2018-11-05 16:51:24 +0700

    Update resources/views/pages/dangky.blade.php

--- resources/views/pages/dangky.blade.php
+++ resources/views/pages/dangky.blade.php
@@ -3,39 +3,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>dang-ky</title>
-	<script type="text/javascript" src="resources/js/jquery.min.js"></script>
-	<script type="text/javascript" src="resources/js/bootstrap.min.js"></script>
-	<script type="text/javascript" src="resources/js/function.js"></script>
-	<link rel="stylesheet" type="text/css" href="resources/css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+	<script type="text/javascript" src="js/jquery.min.js"></script>
+	<script type="text/javascript" src="js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="js/function.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<div class="cha">
-		<img src="resources/images/hinhnen.jpg" class="img-fluid banner" alt="">
+		<img src="images/hinhnen.jpg" class="img-fluid banner" alt="">
 		<div class="container">
-			<form action="" class="form-dk">
+			<form action="dangky" method="post" class="form-dk">
 				<div class="container">
 					<div class="row">
+						<input type="hidden" name="_token" value="{{csrf_token()}}">
 						<div class="col-md-12 img-nen">
-							<img src="resources/images/logo12.png" class="img-hinh" alt="">
+							<img src="images/logo12.png" class="img-hinh" alt="">
+						</div>
+
+						<div class="col-md-12 theInput">
+							<input type="text" placeholder="Tên đăng nhập" name="username" ></input>
 						</div>
 						<div class="col-md-12 theInput">
-							<input type="text" placeholder="Tên đăng nhập" ></input>
+							<input placeholder="Mật khẩu" type="Password" name="pass" >
 						</div>
 						<div class="col-md-12 theInput">
-							<input placeholder="Mật khẩu" type="Password" >
+							<input placeholder="Nhập lại mật khẩu" type="Password" name="pass1" >
 						</div>
 						<div class="col-md-12 theInput">
-							<input placeholder="Nhập lại mật khẩu" type="Password" >
+							<input type="text" placeholder="Họ và tên" name="ten" ></input>
 						</div>
 						<div class="col-md-12 theInput">
-							<input type="text" placeholder="Họ và tên" ></input>
+							<input type="email"  placeholder="Email" name="email"></input>
 						</div>
 						<div class="col-md-12 theInput">
-							<input type="email"  placeholder="Email" ></input>
-						</div>
-						<div class="col-md-12 theInput">
-							<input type="text" placeholder="Số điện thoại" ></input>
+							<input type="text" placeholder="Số điện thoại" name="sdt" ></input>
 						</div>
 						<div class="col-md-12 nutSubmit">
 							<input type="submit" value="Đăng ký"></div>	
@@ -44,5 +46,5 @@
 				</form>
 			</div>
 		</div>
-</body>
-</html>
+	</body>
+	</html>
