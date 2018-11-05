d55e90508cb3ae4b2b674dbe72439a5527fb8ccc
Hao <kehao12@gmail.com>
2018-11-05 16:37:47 +0700

    Update resources/views/pages/dangky.blade.php

--- resources/views/pages/dangky.blade.php
+++ resources/views/pages/dangky.blade.php
@@ -1,1 +1,48 @@
-dangky.blade.php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>dang-ky</title>
+	<script type="text/javascript" src="resources/js/jquery.min.js"></script>
+	<script type="text/javascript" src="resources/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="resources/js/function.js"></script>
+	<link rel="stylesheet" type="text/css" href="resources/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="resources/css/style.css">
+</head>
+<body>
+	<div class="cha">
+		<img src="resources/images/hinhnen.jpg" class="img-fluid banner" alt="">
+		<div class="container">
+			<form action="" class="form-dk">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-12 img-nen">
+							<img src="resources/images/logo12.png" class="img-hinh" alt="">
+						</div>
+						<div class="col-md-12 theInput">
+							<input type="text" placeholder="Tên đăng nhập" ></input>
+						</div>
+						<div class="col-md-12 theInput">
+							<input placeholder="Mật khẩu" type="Password" >
+						</div>
+						<div class="col-md-12 theInput">
+							<input placeholder="Nhập lại mật khẩu" type="Password" >
+						</div>
+						<div class="col-md-12 theInput">
+							<input type="text" placeholder="Họ và tên" ></input>
+						</div>
+						<div class="col-md-12 theInput">
+							<input type="email"  placeholder="Email" ></input>
+						</div>
+						<div class="col-md-12 theInput">
+							<input type="text" placeholder="Số điện thoại" ></input>
+						</div>
+						<div class="col-md-12 nutSubmit">
+							<input type="submit" value="Đăng ký"></div>	
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+</body>
+</html>
